3506f1f7e9c7a3862cdc357b3dba856b6e5d96e8
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-02-26 16:06:49 +0530

    description field added to jira spring boot demo

--- src/main/java/com/jira/controller/TaskController.java
+++ src/main/java/com/jira/controller/TaskController.java
@@ -1,2 +1,28 @@
-package com.jira.controller;public class TaskController {
+package com.jira.controller;
+
+import com.jira.entity.IssueRequest;
+import com.jira.entity.Response;
+import com.jira.service.TaskService;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class TaskController {
+
+    private TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/create/issue")
+    public ResponseEntity<Response> createIssue(@RequestBody IssueRequest request) throws UnirestException, IOException {
+        return new ResponseEntity<>(taskService.createIssue(request), HttpStatus.CREATED);
+    }
+
 }
